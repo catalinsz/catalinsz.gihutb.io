1914df5b61fe1b0a8c0323e4c1c04bed8fadfad1
catalinszilagyi <szilagyi.catalin@yahoo.com>
2023-03-29 21:00:42 +0300

    toggle menu pages

---
+++ index.js
@@ -0,0 +1,8 @@
+var homePage = document.getElementById("home");
+var skillsPage = document.getElementById("skills");
+homePage.style.display = "block";
+
+function showSkills() {
+  homePage.style.display = "none";
+  skillsPage.style.display = "block";
+}
