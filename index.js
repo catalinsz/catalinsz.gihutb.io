4e415e459b076271db561ffb721d9df0136a857c
catalinszilagyi <szilagyi.catalin@yahoo.com>
2023-04-05 16:36:38 +0300

    update code to be the same

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
-let activePage = "home";
+// let activePage = "home";
 
-function hidePreviousPage(id) {
-  const el = document.getElementById(id);
-  el.style.display = "none";
+// function hidePreviousPage(id) {
+//   const el = document.getElementById(id);
+//   el.style.display = "none";
+// }
+
+// function showPage(pageId) {
+//   hidePreviousPage(activePage);
+//   const el = document
+//     .querySelector("#top-menu-bar")
+//     .addEventListener("click", function (e) {
+//       console.log(e.target.dataset.page);
+//     });
+//   document.getElementById(pageId).style.display = "block";
+//   activePage = pageId;
+// }
+
+var activePage = "skills";
+
+show(activePage);
+
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
+function show(id) {
+  document.getElementById(id).style.display = "block";
 }
 
-function showPage(pageId) {
-  hidePreviousPage(activePage);
-  document.getElementById(pageId).style.display = "block";
-  activePage = pageId;
+function hideAllPages() {
+  var pages = document.querySelectorAll("#main .page");
+  pages.forEach(function (page) {
+    hide(page.id);
+  });
 }
+
+function showPage(id) {
+  hideAllPages();
+  show(id);
+}
+
+document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+  if (e.target.matches("a")) {
+    var id = e.target.dataset.page;
+    showPage(id);
+  }
+});
+
+function showSkills() {
+  var skills = [
+    {
+      name: "HTML",
+      endorcements: 5,
+    },
+    {
+      name: "CSS",
+      endorcements: 10,
+    },
+    {
+      name: "JS",
+      endorcements: 15,
+    },
+  ];
+  var html = skills.map(function (skill) {
+    return `<li>${skill.name} - <span class="endorcements">${skill.endorcements}</span></li>`;
+  });
+
+  var container = document.querySelector("#skills ul");
+  container.innerHTML = html.join("");
+}
+
+showSkills();
